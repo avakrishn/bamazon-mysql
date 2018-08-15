10393f89980d9bc5a0ecd88203cfbd111cd588c2
ava krishn <avakrishn@gmail.com>
2018-08-14 22:36:00 -0700

    fix indentation

--- bamazon.sql
+++ bamazon.sql
@@ -4,12 +4,12 @@ CREATE DATABASE bamazon_db;
 
 USE bamazon_db;
 
-CREATE TABLE products(
+CREATE TABLE products (
 	item_id INT NOT NULL AUTO_INCREMENT,
 	product_name VARCHAR(255) NOT NULL,
     department_name VARCHAR(255) NOT NULL,
     price DECIMAL(10,2) NULL,
-    stock_quantity INT NOT NULL
+    stock_quantity INT NOT NULL,
 	PRIMARY KEY (item_id)
 );
 
