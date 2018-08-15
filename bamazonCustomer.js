0326fb976b9557eb01f3bd25d58b462354ad7f52
ava krishn <avakrishn@gmail.com>
2018-08-14 22:39:05 -0700

    install and require npm packages

---
+++ bamazonCustomer.js
@@ -0,0 +1,7 @@
+//Install and Require mySQL and Inquirer npm packages
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+//Read and set any environment variables with the dotenv package.
+require("dotenv").config();
+
